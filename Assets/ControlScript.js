d7e130899ccbe8c57d3da3dedfb76706eae47cf0
Jonathan <jstacy3413@gmail.com>
2013-08-09 23:07:00 -0500

    Add suspected complete set of files necessary to build project

---
+++ Assets/ControlScript.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var forward_speed : float = 3;
+var turn_speed : float = 2;
+
+
+function Start () {
+
+}
+
+function Update () 
+{
+    var forward_move_amount = Input.GetAxis( "Vertical" ) * forward_speed;
+    var turn_move_amount = Input.GetAxis( "Horizontal" ) * turn_speed;
+
+    transform.Rotate( 0, turn_move_amount, 0 );
+
+    rigidbody.AddRelativeForce( -forward_move_amount, 0, 0 );
+}
